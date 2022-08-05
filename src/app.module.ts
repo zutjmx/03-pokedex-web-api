f11e15d2c9880614ac75ba44da0e6536b23632f9
Jesus Zuñiga <jesuszt@hotmail.com>
2022-08-05 17:05:10 -0500

    Fin de la sección 7.-MongoDB Pokedex

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    PokemonModule
+
+      MongooseModule.forRoot('mongodb://localhost:27017/nest_pokemon'),
+
+    PokemonModule,
+
+    CommonModule
   ],
 })
 export class AppModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,26 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    
+    try {
+      
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+
+    } catch (error) {
+      this.manejoDerror(error);
+    }
+
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  async findAll() {
+    const pokemons = await this.pokemonModel.find();
+    return pokemons;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if(!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({no: term});
+    }
+
+    if(!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    if(!pokemon) {
+      pokemon = await this.pokemonModel.findOne({name: term.toLowerCase().trim()});
+    }
+
+    if(!pokemon) {
+      throw new NotFoundException(`:: No se encontró un pokemon con termino de búsqueda '${term}' ::`);
+    }
+
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    
+    if(updatePokemonDto.name) {
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+    }
+
+    try {
+
+      await pokemon.updateOne(updatePokemonDto);
+      return {...pokemon.toJSON(), ...updatePokemonDto};
+
+    } catch (error) {
+      this.manejoDerror(error);
+    }
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    /* const pokemon = await this.findOne(id);
+    await pokemon.deleteOne(); */
+    
+    //const resultado = await this.pokemonModel.findByIdAndDelete(id);
+    
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+
+    if(deletedCount===0) {
+      throw new NotFoundException(`:: No se sencontró un registro con id '${id}' ::`);
+      
+    }
+
+    return {id,deletedCount};
   }
+
+  //! Método para manejo de error.
+  private manejoDerror(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(`:: El valor '${JSON.stringify(error.keyValue)}' ya existe en la BD ::`);
+    }
+
+    console.log(error);
+    throw new InternalServerErrorException(":: Error al guardar en la BD [verificar log's] ::");
+  }
+
 }
